73e28b6368ce23d6be0b43a2733ce3d86a9eab44
chandrumathiyazhagan <chandruniveen@gmail.com>
2024-05-09 13:35:59 +0530

    aaa

---
+++ program.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playercontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    public GameObject WinText;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "sphere")
+        {
+            Destroy(collision.gameObject);
+            WinText.SetActive(true);
+        }
+    }
+}
